ce754025c2360cedd108931c6987f84b14dac1b5
1010 <134185805+jedenzero@users.noreply.github.com>
2024-09-11 18:13:40 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const name = new URLSearchParams(location.search).get('doc');
+let list = [];
 
 if(name){
   //문서 내용 가지고 오기
   fetch(`https://raw.githubusercontent.com/jedenzero/alcze/main/docs/${name}`)
     .then(response => response.text())
     .then(data => {
-      document.querySelector('#content').innerHTML = marked.parse(data);
+        document.querySelector('#content').innerHTML = `<h1>${name}</h1>`;
+        document.querySelector('#content').innerHTML += marked.parse(data);
+    })
+    .catch(() => {
+        document.querySelector('#content').innerHTML = `<div class="danger">존재하지 않는 문서입니다.</div>`;
     });
 }
 
 //문서 목록 가지고 오기
 fetch(`https://api.github.com/repos/jedenzero/alcze/contents/docs`)
   .then(response => response.json())
-  .then(data => {const list = data;});
+  .then(data => {list = data;});
 
 //문서 검색
