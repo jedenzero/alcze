371dca50808f3986ffe7256a6e42608cc7b1282b
1010 <134185805+jedenzero@users.noreply.github.com>
2024-10-09 01:58:52 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,57 +1,28 @@
-const name = window.location.pathname.slice(1);
-let list = [];
+import React, { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
+import marked from 'marked';
 
-if(name){
-  //문서 내용 가지고 오기
-  fetch(`https://raw.githubusercontent.com/jedenzero/alcze/main/docs/${name}.md`)
-    .then(response => response.text())
-    .then(data => {
-        document.querySelector('#content').innerHTML = `<h1>${name}</h1>`;
-        document.querySelector('#content').innerHTML += parse(data);
-    })
-    .catch(() => {
-        document.querySelector('#content').innerHTML = `<div class="danger">존재하지 않는 문서입니다.</div>`;
-    });
+function Content() {
+    const [content, setContent] = useState('');
+    const name = window.location.pathname.slice(1);
+
+    useEffect(() => {
+        fetch(`https://raw.githubusercontent.com/jedenzero/alcze/main/docs/${name}.md`)
+            .then(response => {
+                if (!response.ok) throw new Error('문서를 불러오는 데 실패했습니다.');
+                return response.text();
+            })
+            .then(data => {
+                setContent(marked(data));
+            })
+            .catch(() => {
+                setContent('<div class="danger">존재하지 않는 문서입니다.</div>');
+            });
+    }, [name]);
+
+    return (
+        <div dangerouslySetInnerHTML={{__html:content }}/>
+    );
 }
 
-//문서 목록 가지고 오기
-fetch(`https://api.github.com/repos/jedenzero/alcze/contents/docs`)
-  .then(response => response.json())
-  .then(data => {
-    list = data.map(obj => obj['name'].replace('.md', ''));
-  });
-
-//문서 검색
-function search(){
-    document.querySelector('#result').innerHTML = '';
-    
-    const keyword = document.querySelector('input').value;
-    let result = list.filter(doc => doc.includes(keyword));
-    
-    result.sort((a,b) => a.length-b.length || a.localeCompare(b));
-    result = result.slice(0,5);
-    result.forEach(doc => {
-        document.querySelector('#result').innerHTML += `<a href="./?doc=${doc}"><div>${doc}</div></a>`;
-    });
-}
-
-function parse(doc){
-  if(doc.match(/^<<[^\n>]+>>\n/)){
-    let theme = '';
-    doc = doc.replace(/^<<([^\n>]+)>>\n/,(match, captured_theme)=>{
-      theme = captured_theme;
-      return '';
-    });
-    
-    const theme_link = document.createElement('link');
-    theme_link.rel = 'stylesheet';
-    theme_link.href = `./themes/${theme}.css`;
-    document.head.appendChild(theme_link);
-  }
-  doc = marked.parse(doc);
-  doc = doc.replace(/<a href="([^"]+)">/g, '<a href="./$1">');
-  doc = doc.replace(/(?<=<a href="[^"]*)_(?=[^">]*">)/g, ' ');
-  doc = doc.replace(/\[\[([^\[\]\n]+)\]\]/g, '<a href="./$1">$1</a>');
-  doc = doc.replace(/<img src="([^"]+)"/g, '<img src="./imgs/$1"');
-  return doc;
-}
+ReactDOM.render(<Content/>, document.querySelector('#content'));
