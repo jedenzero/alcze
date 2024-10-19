b942752dd883d64228302d19661cebddb3d2ff4a
1010 <134185805+jedenzero@users.noreply.github.com>
2024-10-19 23:03:19 +0900

    Update [docName].js

--- pages/[docName].js
+++ pages/[docName].js
@@ -56,6 +56,7 @@ export default function Doc() {
         <div>
             <Head>
                 <title>{docName} - ✨alcze</title>
+                <meta name="google-site-verification" content="Fb4dZfbsUFfHNH2547L2XC-Rc_80cWN8gcomQge5t7k"/>
             </Head>
             <div id="navbar">
                 <div id="logo">
